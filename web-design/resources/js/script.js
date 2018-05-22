4326083e7277f1b0aaf04ef78232f2c281358b78
jessicamor <jessy@anang-homes-MacBook-Pro.local>
2018-05-23 05:58:00 +0700

    Update the design
    update the design

--- web-design/resources/js/script.js
+++ web-design/resources/js/script.js
@@ -1,16 +1,20 @@
+//owl carousel responsive
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel({
         items: 1,
-        loop: false,
+        loop: true,
         dots: true,
         margin: 10,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
         responsiveClass: true,
         responsive: {
             0: {
                 items: 1,
             },
             659: {
-                items: 2, 
+                items: 2,
             },
             1000: {
                 items: 2,
